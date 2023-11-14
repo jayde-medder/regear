29f5409a21272b79c42c6f4a18ff6bb5f615f871
Jayde Medder <jayde.medder@hotmail.com>
2023-11-14 15:04:17 +1300

    add superagent, define custom hooks

--- client/apis/apiClient.ts
+++ client/apis/apiClient.ts
@@ -1,6 +1,38 @@
 import request from 'superagent'
+import type { Post, PostData } from '../../models/posts'
 
-export async function getGreeting(): Promise<string> {
-  const res = await request.get('/api/v1/greeting')
-  return res.body.greeting
+const rootUrl = '/api/v1'
+
+export async function getAllposts(): Promise<Post[]> {
+  const res = await request.get(rootUrl + '/posts')
+  if (res.status !== 200) {
+    throw new Error('Failed to fetch posts')
+  }
+  return res.body.posts as Post[]
+}
+
+export async function getpostById(id: number): Promise<Post | undefined> {
+  const res = await request.get(rootUrl + `/posts/${id}`)
+  if (res.status !== 200) {
+    throw new Error(`Failed to fetch post with ID ${id}`)
+  }
+  return res.body.post
+}
+
+export async function updatePost({
+  id,
+  post,
+}: {
+  id: number
+  post: Post
+}): Promise<void> {
+  await request.patch(rootUrl + `/posts/${id}`).send(post)
+}
+
+export async function deletePost(id: number): Promise<void> {
+  await request.delete(rootUrl + `/posts/${id}`)
+}
+
+export async function addPost(newPost: PostData): Promise<void> {
+  await request.post(rootUrl + '/posts').send({ ...newPost })
 }
---
+++ client/hooks/usePost.ts
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { addPost, updatePost, deletePost } from '../apis/apiClient'
+
+export function usePost() {
+  const queryClient = useQueryClient()
+
+  const updatePostMutation = useMutation(updatePost, {
+    onSuccess: async () => {
+      queryClient.invalidateQueries(['post'])
+    },
+  })
+
+  const deletePostMutation = useMutation(deletePost, {
+    onSuccess: async () => {
+      queryClient.invalidateQueries(['post'])
+    },
+  })
+
+  const addPostMutation = useMutation(addPost, {
+    onSuccess: async () => {
+      queryClient.invalidateQueries(['post'])
+    },
+  })
+
+  return {
+    updatePostMutation,
+    deletePostMutation,
+    addPostMutation,
+  }
+}

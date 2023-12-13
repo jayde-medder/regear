5b5f235f76cf7881d606cbda2ff3dacb9d8920ce
Gabriel Rawcliffe <gabriel.rawcliffe@gmail.com>
2023-12-13 13:37:15 +1300

    start of useQuery for inventory

--- client/components/Inventory/Inventory.tsx
+++ client/components/Inventory/Inventory.tsx
@@ -1,6 +1,28 @@
+import { useQuery } from '@tanstack/react-query'
 import styles from './Inventory.module.css'
 
 function Inventory() {
+  const {
+    data: inventory,
+    isLoading,
+    isError,
+  } = useQuery(['inventory'], () => getInventory())
+  if (isError)
+    return (
+      <>
+        <h2>
+          There seems to be a problem accessing the inventory. Please send us a
+          request
+        </h2>
+      </>
+    )
+  if (isLoading)
+    return (
+      <>
+        {/* add loading animation */}
+        <h2>...Loading </h2>
+      </>
+    )
   return (
     <div>
       <h1>Inventory</h1>

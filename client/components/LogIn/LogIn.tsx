92e84d6a0e07b7740409fd266f380980f60d15c3
Jayde Medder <jayde.medder@hotmail.com>
2023-12-16 14:20:57 +1300

    Authenticated componenent added

--- client/components/LogIn/LogIn.tsx
+++ client/components/LogIn/LogIn.tsx
@@ -1,16 +1,43 @@
 import styles from './LogIn.module.css'
+import { useAuth0 } from '@auth0/auth0-react'
+import {
+  IfAuthenticated,
+  IfNotAuthenticated,
+} from '../Authenticated/Authenticated'
 
 function LogIn() {
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  const handleSignOut = () => {
+    return logout()
+  }
+
+  const handleSignIn = () => {
+    return loginWithRedirect()
+  }
+
   return (
     <div className={styles['log-in-wrapper']}>
-      <a
-        id={styles['log-in-button']}
-        href="https://react.school"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <button>LOG-IN/SIGN-UP</button>
-      </a>
+      <IfAuthenticated>
+        <a
+          id={styles['log-in-button']}
+          href="https://react.school"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button>LOG-IN/SIGN-UP</button>
+        </a>
+      </IfAuthenticated>
+      <IfNotAuthenticated>
+        <a
+          id={styles['log-in-button']}
+          href="https://react.school"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button>LOG-IN/SIGN-UP</button>
+        </a>
+      </IfNotAuthenticated>
     </div>
   )
 }

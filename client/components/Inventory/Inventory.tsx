e3db58e7686f1f6d4af652ec4d973572fcd18d66
Gabriel Rawcliffe <135697253+gabriel-rawcliffe@users.noreply.github.com>
2023-12-20 19:23:38 +1300

    adds basic inventory display options

--- client/components/Inventory/Inventory.tsx
+++ client/components/Inventory/Inventory.tsx
@@ -3,7 +3,10 @@ import styles from './Inventory.module.css'
 import { getInventoryList } from '../../apis/apiInventory'
 import { ItemList } from '../../../models/inventory'
 import { useState } from 'react'
-import ItemOrder from '../ItemOrder/ItemOrder'
+import ItemOrder from '../ItemOrder/ItemDisplayForm'
+import CategoryDisplay from '../CategoryDisplay/CategoryDisplay'
+import AlphabeticalDisplay from '../AlphabeticalDisplay/AlphabeticalDisplay'
+import DateAddedDisplay from '../DateAddedDisplay/DateAddedDisplay'
 
 function Inventory() {
   const {
@@ -36,98 +39,15 @@ function Inventory() {
       </>
     )
 
-  //Maps items into categories, so that they can be displayed by category if desired. Might need to alter the approach for conditional rendering of a subset of categories, but should work I hope.
-  const organizeData = (inventory: ItemList[]) => {
-    const categoriesMap = new Map()
-
-    const processCategory = (
-      item: ItemList,
-      parentCategory: {
-        subcategories: {
-          id: number
-          name: string
-          items: ItemList[]
-          subcategories: never[]
-        }[]
-      }
-    ) => {
-      const category = parentCategory.subcategories.find(
-        (sub) => sub.id === item.category_id
-      )
-
-      if (category) {
-        category.items.push(item)
-      } else {
-        parentCategory.subcategories.push({
-          id: item.category_id,
-          name: `${item.category_name}`,
-          items: [item],
-          subcategories: [],
-        })
-      }
-    }
-
-    inventory.forEach((item) => {
-      if (item.parent_id) {
-        const parentCategory = categoriesMap.get(item.parent_id) || {
-          subcategories: [],
-        }
-        processCategory(item, parentCategory)
-        categoriesMap.set(item.parent_id, parentCategory)
-      } else {
-        const category = categoriesMap.get(item.category_id) || {
-          id: item.category_id,
-          name: `${item.category_name}`,
-          items: [],
-          subcategories: [],
-        }
-        category.items.push(item)
-        categoriesMap.set(item.category_id, category)
-      }
-    })
-
-    console.log(Array.from(categoriesMap.values())) // Log the data structure
-
-    return Array.from(categoriesMap.values())
-  }
-
-  const renderCategory = (category: {
-    id: number
-    name: string
-    items: ItemList[]
-    subcategories: any[]
-  }) => (
-    <div key={category.id}>
-      <h2>{category.name}</h2>
-      {category.items.map((item) => (
-        <div key={item.id}>
-          <p>{item.name}</p>
-        </div>
-      ))}
-      {/* Render items for subcategories */}
-      {category.subcategories.map((subcategory) => (
-        <div key={subcategory.id}>
-          <h3>{subcategory.name}</h3>
-          {subcategory.items.map((item: ItemList) => (
-            <div key={item.id}>
-              <p>{item.name}</p>
-              {/* Add more item details as needed */}
-            </div>
-          ))}
-          {/* Recursively render subcategories */}
-          {subcategory.subcategories.map(renderCategory)}
-        </div>
-      ))}
-    </div>
-  )
-
   return (
     <>
       <ItemOrder
         itemOrder={itemOrder}
         handleSelectChange={handleSelectChange}
       />
-      <div>{organizeData(inventory).map(renderCategory)}</div>
+      {itemOrder === 'A-Z' && <AlphabeticalDisplay inventory={inventory} />}
+      {itemOrder === 'Date added' && <DateAddedDisplay inventory={inventory} />}
+      {itemOrder === 'Category' && <CategoryDisplay inventory={inventory} />}
     </>
   )
 }

dde918a3751dfd0bc8c883207d81da85d23b137d
Jayde Medder <jayde.medder@hotmail.com>
2024-06-06 19:20:30 +1200

    ordering migrations

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -6,21 +6,13 @@ const __dirname = Path.dirname(__filename)
 
 export default {
   development: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+    client: 'pg',
     connection: {
-      filename: Path.join(__dirname, 'dev.sqlite3'),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
-  },
-
-  test: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: ':memory:',
+      host: '127.0.0.1',
+      user: 'jayde',
+      password: 'Thix7cpip4E',
+      database: 'regear',
+      port: 5432,
     },
     migrations: {
       directory: Path.join(__dirname, 'migrations'),
@@ -28,19 +20,27 @@ export default {
     seeds: {
       directory: Path.join(__dirname, 'seeds'),
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: Path.join(__dirname, 'migrations'),
+    },
+    seeds: {
+      directory: Path.join(__dirname, 'seeds'),
     },
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: '/app/storage/prod.sqlite3',
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: Path.join(__dirname, 'migrations'),
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    seeds: {
+      directory: Path.join(__dirname, 'seeds'),
     },
   },
 }

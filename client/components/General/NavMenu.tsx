9a50a61e9f2b84f391ec614e4f3c502e6e16b7ac
Jayde Medder <jayde.medder@hotmail.com>
2024-07-01 16:30:29 +1200

    about page text

--- client/components/General/NavMenu.tsx
+++ client/components/General/NavMenu.tsx
@@ -24,6 +24,31 @@ export function NavMenu() {
         <NavigationMenuList className=" border-black border-b border-l border-r rounded-b-md">
           <NavigationMenuItem className="border-black border-r">
             <NavigationMenuTrigger>
+              <h2 className="hidden sm:block">Borrow</h2>
+              <HeartHandshake color="green" className="h-5 sm:h-3" />
+            </NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <div className="grid gap-3 p-6 md:w-[400px] lg:w-[500px]">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
+                  >
+                    <HeartHandshake color="green" />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      Borrow from the Hardware Library
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      View our inventory and send a request to borrow an item.
+                      We&apos;re flexible on how long you can borrow for.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </div>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="border-black border-r">
+            <NavigationMenuTrigger>
               <p className="hidden sm:block">Fix</p>
               <Wrench color="red" className="h-5 sm:h-3" />
             </NavigationMenuTrigger>
@@ -57,31 +82,6 @@ export function NavMenu() {
                   broken.
                 </ListItem>
               </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-          <NavigationMenuItem className="border-black border-r">
-            <NavigationMenuTrigger>
-              <p className="hidden sm:block">Borrow</p>
-              <HeartHandshake color="green" className="h-5 sm:h-3" />
-            </NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <div className="grid gap-3 p-6 md:w-[400px] lg:w-[500px]">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <HeartHandshake color="green" />
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Borrow from the Hardware Library
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      View our inventory and send a request to borrow an item.
-                      We&apos;re flexible on how long you can borrow for.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </div>
             </NavigationMenuContent>
           </NavigationMenuItem>
           <NavigationMenuItem>
